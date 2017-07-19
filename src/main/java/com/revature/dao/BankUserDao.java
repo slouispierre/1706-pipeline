37f927d573be51d1394fd234e17d6a214ce1fd5b
slouispierre <sam.louispierre@gmail.com>
2017-07-19 10:19:16 -0400

    adding BankingProject

---
+++ src/main/java/com/revature/dao/BankUserDao.java
@@ -0,0 +1,26 @@
+package com.revature.dao;
+
+import com.revature.model.BankUser;
+
+public interface BankUserDao {
+
+	/*
+	 * 	CRUD ops only
+	 * 
+	 */
+	
+	
+	//create
+	
+	
+	
+	//read
+	public BankUser getBankUserByUsername(BankUser user);
+	
+	//update
+	
+	
+	//delete
+	
+	
+}
---
+++ src/main/java/com/revature/service/AppService.java
@@ -0,0 +1,23 @@
+package com.revature.service;
+
+import com.revature.dao.BankUserDao;
+import com.revature.dao.BankUserDaoImpl;
+import com.revature.model.BankUser;
+
+public class AppService {
+
+	public BankUser validateUser(BankUser user) {
+
+		BankUserDao dao = new BankUserDaoImpl();
+
+		BankUser dbUser = dao.getBankUserByUsername(user);
+		if (dbUser != null) {
+			if (dbUser.getUsername().equals(user.getUsername()) && dbUser.getPassword().equals(user.getPassword())) {
+				return dbUser;
+			}
+		}
+		return null;
+
+	}
+
+}
